49ee962f05d2fd183d878a9f53dde05ed9cf1c3d
Earl Manley <ewmanley02@yahoo.com>
2021-02-19 22:24:24 -0500

    First Commit

---
+++ src/test/java/CrystalBudget/springcloudconfigservercrystalbudget/SpringCloudConfigServerCrystalbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package CrystalBudget.springcloudconfigservercrystalbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerCrystalbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
